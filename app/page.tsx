12c7a66099d251d8941b429ba21177c3d01327d7
AyaHusein2001 <aya.ahmed2001.aa@gmail.com>
2024-11-20 17:45:19 +0200

    about section finished

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,17 @@
+import About from "@/components/About";
 import FeaturedProjects from "@/components/FeaturedProjects";
 import Hero from "@/components/Hero";
 import { Box, Divider } from "@mui/material";
 
 export default function Home() {
   return (
-    
     <Box>
       <Hero />
-      <Divider sx={{borderColor:'#484848'}} />
-      
-<FeaturedProjects />
+      <Divider sx={{ borderColor: "#484848" }} />
+
+      <FeaturedProjects />
+      <Divider sx={{ borderColor: "#484848" }} />
+<About />
     </Box>
   );
 }
